60d6586f175ce4728e01542b05b5f6c2e67c6a15
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-08-04 15:16:59 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -3,11 +3,11 @@ function licz(){
     var b = document.getElementById("2").value;
     var wynik = document.getElementById("wynik");
    
-  if(a ==""|| b == ""){
+     if(a ==""|| b == ""){
       wynik.innerHTML="Prosze podać obydwie liczby";   
-  }else{
-  a =parseInt(a);
-  b =parseInt(b);
+     }else{
+    a =parseInt(a);
+    b =parseInt(b);
     
      // wynik.innerHTML="Potrzebujesz:"+" "+b/a +" "+"litrów palia";
       wynik.innerHTML="Potrzebujesz:"+" "+Math.round(b*a/100)+" "+"litrów palia";
